912612877eabcf5285017af2b816fa258e89a838
xiaoweih <xiaowei.huang@live.com>
2019-06-08 21:10:19 +0100

    update

--- MovingObjectDetector/run_detection_main.py
+++ MovingObjectDetector/run_detection_main.py
@@ -59,15 +59,17 @@ input_image_idx = 10
 ROI_centre = [4500, 5000]
 
 wasabi_directory = "/Users/xiaowei/Dropbox/wasabi-detection-python-new/"
-writeimagefolder = "/Users/xiaowei/Dropbox/temporary_buffer/code/savefig/"
+writeimagefolder0 = "/Users/xiaowei/Dropbox/temporary_buffer/code/savefig/"
 
 exampleString = "%s_%s_%s/"%(input_image_idx,ROI_centre[0],ROI_centre[1])
-createImageDirectory(writeimagefolder+exampleString)
+createImageDirectory(writeimagefolder0+exampleString)
 
 if attack: 
-    createImageDirectory(writeimagefolder+exampleString+"attacked")
+    writeimagefolder = writeimagefolder0+exampleString+"attacked"
 else: 
-    createImageDirectory(writeimagefolder+exampleString+"original")
+    writeimagefolder = writeimagefolder0+exampleString+"original"
+    
+createImageDirectory(writeimagefolder)
 
 
 ## to run the WAMI tracker
