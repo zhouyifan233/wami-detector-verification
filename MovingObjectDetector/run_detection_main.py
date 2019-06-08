942edefe2e444f7ebea0a755aa1ab452d520e3a3
xiaoweih <xiaowei.huang@live.com>
2019-06-08 21:16:17 +0100

    update

--- MovingObjectDetector/run_detection_main.py
+++ MovingObjectDetector/run_detection_main.py
@@ -58,17 +58,16 @@ attack = False
 input_image_idx = 10
 ROI_centre = [4500, 5000]
 
-imagefolder = wasabi_directory+"WAPAFB_images_train/training/"
-wasabi_directory = "/Users/xiaowei/Dropbox/wasabi-detection-python-new/"
+imagefolder = "/Users/xiaowei/Dropbox/wasabi-detection-python-new/WAPAFB_images_train/training/"
 writeimagefolder0 = "/Users/xiaowei/Dropbox/temporary_buffer/code/savefig/"
 
 exampleString = "%s_%s_%s/"%(input_image_idx,ROI_centre[0],ROI_centre[1])
 createImageDirectory(writeimagefolder0+exampleString)
 
 if attack: 
-    writeimagefolder = writeimagefolder0+exampleString+"attacked"
+    writeimagefolder = writeimagefolder0+exampleString+"attacked/"
 else: 
-    writeimagefolder = writeimagefolder0+exampleString+"original"
+    writeimagefolder = writeimagefolder0+exampleString+"original/"
     
 createImageDirectory(writeimagefolder)
 
