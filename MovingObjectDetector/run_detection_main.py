7a43da5265c5759b666e34922144dcd80277fb22
xiaoweih <xiaowei.huang@live.com>
2019-06-08 21:38:50 +0100

    update

--- MovingObjectDetector/run_detection_main.py
+++ MovingObjectDetector/run_detection_main.py
@@ -4,13 +4,11 @@ import matplotlib.pyplot as plt
 from BackgroundModel import BackgroundModel
 from DetectionRefinement import DetectionRefinement
 import sys
-sys.path.insert(0,'../TrainNetwork')
 import BaseFunctions as basefunctions
 import timeit
-sys.path.insert(0,'../SimpleTracker')
 from KalmanFilter import KalmanFilter
 from copy import copy
-from BaseFunctions2 import TimePropagate, TimePropagate_, draw_error_ellipse2d, createImageDirectory
+from BaseFunctions2 import TimePropagate, TimePropagate_, draw_error_ellipse2d
 import hdf5storage
 from copy import deepcopy as deepcopy
 import os
@@ -53,168 +51,153 @@ def diff_max(track1, track2):
     
 ####
 
-attack = False 
-
-input_image_idx = 10
-ROI_centre = [4500, 5000]
-
-imagefolder = "/Users/xiaowei/Dropbox/wasabi-detection-python-new/WAPAFB_images_train/training/"
-writeimagefolder0 = "/Users/xiaowei/Dropbox/temporary_buffer/code/savefig/"
-
-exampleString = "%s_%s_%s/"%(input_image_idx,ROI_centre[0],ROI_centre[1])
-createImageDirectory(writeimagefolder0+exampleString)
-
-if attack: 
-    writeimagefolder = writeimagefolder0+exampleString+"attacked/"
-else: 
-    writeimagefolder = writeimagefolder0+exampleString+"original/"
-    
-createImageDirectory(writeimagefolder)
 
 
-## to run the WAMI tracker
-## d_out  : output directory
-## frames : a vector of frames to attack
-ref_track = None
 
-ROI_window = 1000
-image_idx_offset = 0
-num_of_template = 3
-# if not os.path.exists(d_out):
-#  os.makedirs(d_out)
-model_folder = "../Models/"
-model_binary, aveImg_binary, model_regression, aveImg_regression = basefunctions.ReadModels(model_folder)
+def run_detection_main(attack,model_folder,imagefolder,input_image_idx,ROI_centre,writeimagefolder): 
 
-# load transformation matrices
-matlabfile = hdf5storage.loadmat(model_folder+'Data/TransformationMatrices_train.mat')
-TransformationMatrices = matlabfile.get("TransMatrix")
+    ## to run the WAMI tracker
+    ## d_out  : output directory
+    ## frames : a vector of frames to attack
+    ref_track = None
 
-# Load background
-images = []
-for i in range(num_of_template):
-    frame_idx = input_image_idx + image_idx_offset + i - num_of_template
-    ReadImage = cv2.imread(imagefolder + "frame%06d.png" % frame_idx, cv2.IMREAD_GRAYSCALE)
-    ReadImage = ReadImage[ROI_centre[1] - ROI_window:ROI_centre[1] + ROI_window + 1,
-                ROI_centre[0] - ROI_window:ROI_centre[0] + ROI_window + 1]
-    ROI_centre = TimePropagate_(ROI_centre, TransformationMatrices[frame_idx - 1][0])
-    ROI_centre = [int(i) for i in ROI_centre]
-    images.append(ReadImage)
-bgt = BackgroundModel(num_of_template=num_of_template, templates=images)
+    ROI_window = 1000
+    image_idx_offset = 0
+    num_of_template = 3
+    # if not os.path.exists(d_out):
+    #  os.makedirs(d_out)
+    model_binary, aveImg_binary, model_regression, aveImg_regression = basefunctions.ReadModels(model_folder)
 
-# initialise Kalman filter
-kf = KalmanFilter(np.array([[722], [1487], [0], [0]]), np.diag([900, 900, 400, 400]), 5, 6)
-# kf1 = KalmanFilter(np.array([[2989], [1961], [0], [0]]), np.diag([900, 900, 400, 400]), 5, 6)
-detections_all = []
-refinementID = None
-detected_track = []
-for i in range(20):
-    starttime = timeit.default_timer()
-    # Read input image
-    frame_idx = input_image_idx + image_idx_offset + i
-    ReadImage = cv2.imread(imagefolder + "frame%06d.png" % frame_idx, cv2.IMREAD_GRAYSCALE)
-    input_image = ReadImage[ROI_centre[1] - ROI_window:ROI_centre[1] + ROI_window + 1,
-                  ROI_centre[0] - ROI_window:ROI_centre[0] + ROI_window + 1]
-    ROI_centre = TimePropagate_(ROI_centre, TransformationMatrices[frame_idx - 1][0])
-    ROI_centre = [int(i) for i in ROI_centre]
+    # load transformation matrices
+    matlabfile = hdf5storage.loadmat(model_folder+'Data/TransformationMatrices_train.mat')
+    TransformationMatrices = matlabfile.get("TransMatrix")
 
-    Hs = bgt.doCalculateHomography(input_image)
-    bgt.doMotionCompensation(Hs, input_image.shape)
-    BackgroundSubtractionCentres, BackgroundSubtractionProperties = bgt.doBackgroundSubtraction(input_image, thres=8)
+    # Load background
+    images = []
+    for i in range(num_of_template):
+        frame_idx = input_image_idx + image_idx_offset + i - num_of_template
+        ReadImage = cv2.imread(imagefolder + "frame%06d.png" % frame_idx, cv2.IMREAD_GRAYSCALE)
+        ReadImage = ReadImage[ROI_centre[1] - ROI_window:ROI_centre[1] + ROI_window + 1,
+                    ROI_centre[0] - ROI_window:ROI_centre[0] + ROI_window + 1]
+        ROI_centre = TimePropagate_(ROI_centre, TransformationMatrices[frame_idx - 1][0])
+        ROI_centre = [int(i) for i in ROI_centre]
+        images.append(ReadImage)
+    bgt = BackgroundModel(num_of_template=num_of_template, templates=images)
 
-    dr = DetectionRefinement(input_image, bgt.getCompensatedImages(), BackgroundSubtractionCentres,
-                             BackgroundSubtractionProperties, model_binary, aveImg_binary, model_regression,
-                             aveImg_regression,attack)
-    # dr.refinementID=refinementID
-    refinedDetections, refinedProperties = dr.doMovingVehicleRefinement()
-    regressedDetections = dr.doMovingVehiclePositionRegression()
-    regressedDetections = np.asarray(regressedDetections)
+    # initialise Kalman filter
+    kf = KalmanFilter(np.array([[722], [1487], [0], [0]]), np.diag([900, 900, 400, 400]), 5, 6)
+    # kf1 = KalmanFilter(np.array([[2989], [1961], [0], [0]]), np.diag([900, 900, 400, 400]), 5, 6)
+    detections_all = []
+    refinementID = None
+    detected_track = []
+    for i in range(20):
+        starttime = timeit.default_timer()
+        # Read input image
+        frame_idx = input_image_idx + image_idx_offset + i
+        ReadImage = cv2.imread(imagefolder + "frame%06d.png" % frame_idx, cv2.IMREAD_GRAYSCALE)
+        input_image = ReadImage[ROI_centre[1] - ROI_window:ROI_centre[1] + ROI_window + 1,
+                      ROI_centre[0] - ROI_window:ROI_centre[0] + ROI_window + 1]
+        ROI_centre = TimePropagate_(ROI_centre, TransformationMatrices[frame_idx - 1][0])
+        ROI_centre = [int(i) for i in ROI_centre]
 
-    # Kalman filter update
-    kf1 = deepcopy(kf)
-    # kf1_flag=False
-    if i > 0:
-        kf.TimePropagate(Hs[num_of_template - 1])
-        kf.predict()
-        """"""
-        # the id in the regressed detections
-        regressionID = kf.NearestNeighbourAssociator(regressedDetections)
-        # the id in the refinement detections (input to the CNN)
-        old_kfz = kf.z
-        # print("regressionID:   ")
-        # print(regressionID)
-        # print("regressedDetectionID:   ")
-        # print(dr.regressedDetectionID)
-        if isinstance(regressionID, np.int64):
-            regression2refinedID = dr.regressedDetectionID[regressionID]
-            refinementID = dr.refinedDetectionsID[regression2refinedID]
-            print("Background subtraction id:" + str(refinementID))
-            print("Background subtraction id type:" + str(type(refinementID)))
-        else:
-            print("Data Association failed (No detection is assigned to this track)...")
-        if isinstance(refinementID, np.int64) and (i > 5):
-            #######
-            #######  here to play 'attack': to call the dr again with refinementID
-            # frame_idx = input_image_idx+image_idx_offset+i
-            # ReadImage = cv2.imread(imagefolder + "frame%06d.png" % frame_idx, cv2.IMREAD_GRAYSCALE)
-            # input_image = ReadImage[ROI_centre[1]-ROI_window:ROI_centre[1]+ROI_window+1, ROI_centre[0]-ROI_window:ROI_centre[0]+ROI_window+1]
-            # ROI_centre = TimePropagate_(ROI_centre, TransformationMatrices[frame_idx-1][0])
-            # ROI_centre = [int(i) for i in ROI_centre]
+        Hs = bgt.doCalculateHomography(input_image)
+        bgt.doMotionCompensation(Hs, input_image.shape)
+        BackgroundSubtractionCentres, BackgroundSubtractionProperties = bgt.doBackgroundSubtraction(input_image, thres=8)
 
-            # Hs = bgt.doCalculateHomography(input_image)
-            # bgt.doMotionCompensation(Hs, input_image.shape)
-            # BackgroundSubtractionCentres, BackgroundSubtractionProperties = bgt.doBackgroundSubtraction(input_image, thres=10)
+        dr = DetectionRefinement(input_image, bgt.getCompensatedImages(), BackgroundSubtractionCentres,
+                                 BackgroundSubtractionProperties, model_binary, aveImg_binary, model_regression,
+                                 aveImg_regression,attack)
+        # dr.refinementID=refinementID
+        refinedDetections, refinedProperties = dr.doMovingVehicleRefinement()
+        regressedDetections = dr.doMovingVehiclePositionRegression()
+        regressedDetections = np.asarray(regressedDetections)
 
-            # dr = DetectionRefinement(input_image, bgt.getCompensatedImages(), BackgroundSubtractionCentres, BackgroundSubtractionProperties, model_binary, aveImg_binary, model_regression, aveImg_regression)
-            #########
-            dr.refinementID = refinementID
-            # dr.refinementID=None
-            refinedDetections, refinedProperties = dr.doMovingVehicleRefinement()
-            regressedDetections = dr.doMovingVehiclePositionRegression()
-            regressedDetections = np.asarray(regressedDetections)
-            dr.refinementID = None
-
-            kf1.TimePropagate(Hs[num_of_template - 1])
-            kf1.predict()
+        # Kalman filter update
+        kf1 = deepcopy(kf)
+        # kf1_flag=False
+        if i > 0:
+            kf.TimePropagate(Hs[num_of_template - 1])
+            kf.predict()
+            """"""
             # the id in the regressed detections
-            print('==== old regressionID', regressionID)
-            regressionID = kf1.NearestNeighbourAssociator(regressedDetections)
-            new_kfz = kf1.z
-            print('*********************')
-            print(old_kfz)
-            print('####')
-            print(new_kfz)
-            print('*********************')
-            print('==== new regressionID', regressionID)
+            regressionID = kf.NearestNeighbourAssociator(regressedDetections)
             # the id in the refinement detections (input to the CNN)
-            print('#### old refinementID', refinementID)
-            if regressionID is None:
-                print('#### new refinementID does not exist, because there is no associated detection')
-            else:
+            old_kfz = kf.z
+            # print("regressionID:   ")
+            # print(regressionID)
+            # print("regressedDetectionID:   ")
+            # print(dr.regressedDetectionID)
+            if isinstance(regressionID, np.int64):
                 regression2refinedID = dr.regressedDetectionID[regressionID]
                 refinementID = dr.refinedDetectionsID[regression2refinedID]
-                print('#### new refinementID', refinementID)
-            # kf1_flag=True
-            kf = deepcopy(kf1)
-            #######
-            #######
+                print("Background subtraction id:" + str(refinementID))
+                print("Background subtraction id type:" + str(type(refinementID)))
+            else:
+                print("Data Association failed (No detection is assigned to this track)...")
+            if isinstance(refinementID, np.int64) and (i > 5):
+                #######
+                #######  here to play 'attack': to call the dr again with refinementID
+                # frame_idx = input_image_idx+image_idx_offset+i
+                # ReadImage = cv2.imread(imagefolder + "frame%06d.png" % frame_idx, cv2.IMREAD_GRAYSCALE)
+                # input_image = ReadImage[ROI_centre[1]-ROI_window:ROI_centre[1]+ROI_window+1, ROI_centre[0]-ROI_window:ROI_centre[0]+ROI_window+1]
+                # ROI_centre = TimePropagate_(ROI_centre, TransformationMatrices[frame_idx-1][0])
+                # ROI_centre = [int(i) for i in ROI_centre]
 
-        # if kf1_flag: kf=deepcopy(kf1)
-        """"""
-        kf.update()
-        trackx = kf.mu_t[0, 0]
-        tracky = kf.mu_t[1, 0]
-        # if kf1_flag:
-        #  kf1.update()
-        #  trackx = kf1.mu_t[0,0]
-        #  tracky = kf1.mu_t[1,0]
-        # propagate all detections
-        detections_all = TimePropagate(detections_all, Hs[num_of_template - 1])
-        detections_all.append(np.array([trackx, tracky]).reshape(2, 1))
-        print('Estimated State: ' + str(kf.mu_t.transpose()))
-    else:
-        trackx = kf.mu_t[0, 0]
-        tracky = kf.mu_t[1, 0]
-        # if kf1_flag:
+                # Hs = bgt.doCalculateHomography(input_image)
+                # bgt.doMotionCompensation(Hs, input_image.shape)
+                # BackgroundSubtractionCentres, BackgroundSubtractionProperties = bgt.doBackgroundSubtraction(input_image, thres=10)
+
+                # dr = DetectionRefinement(input_image, bgt.getCompensatedImages(), BackgroundSubtractionCentres, BackgroundSubtractionProperties, model_binary, aveImg_binary, model_regression, aveImg_regression)
+                #########
+                dr.refinementID = refinementID
+                # dr.refinementID=None
+                refinedDetections, refinedProperties = dr.doMovingVehicleRefinement()
+                regressedDetections = dr.doMovingVehiclePositionRegression()
+                regressedDetections = np.asarray(regressedDetections)
+                dr.refinementID = None
+
+                kf1.TimePropagate(Hs[num_of_template - 1])
+                kf1.predict()
+                # the id in the regressed detections
+                print('==== old regressionID', regressionID)
+                regressionID = kf1.NearestNeighbourAssociator(regressedDetections)
+                new_kfz = kf1.z
+                print('*********************')
+                print(old_kfz)
+                print('####')
+                print(new_kfz)
+                print('*********************')
+                print('==== new regressionID', regressionID)
+                # the id in the refinement detections (input to the CNN)
+                print('#### old refinementID', refinementID)
+                if regressionID is None:
+                    print('#### new refinementID does not exist, because there is no associated detection')
+                else:
+                    regression2refinedID = dr.regressedDetectionID[regressionID]
+                    refinementID = dr.refinedDetectionsID[regression2refinedID]
+                    print('#### new refinementID', refinementID)
+                # kf1_flag=True
+                kf = deepcopy(kf1)
+                #######
+                #######
+
+            # if kf1_flag: kf=deepcopy(kf1)
+            """"""
+            kf.update()
+            trackx = kf.mu_t[0, 0]
+            tracky = kf.mu_t[1, 0]
+            # if kf1_flag:
+            #  kf1.update()
+            #  trackx = kf1.mu_t[0,0]
+            #  tracky = kf1.mu_t[1,0]
+            # propagate all detections
+            detections_all = TimePropagate(detections_all, Hs[num_of_template - 1])
+            detections_all.append(np.array([trackx, tracky]).reshape(2, 1))
+            print('Estimated State: ' + str(kf.mu_t.transpose()))
+        else:
+            trackx = kf.mu_t[0, 0]
+            tracky = kf.mu_t[1, 0]
+            # if kf1_flag:
         #  trackx = kf1.mu_t[0,0]
         #  tracky = kf1.mu_t[1,0]
         detections_all.append(np.array([trackx, tracky]).reshape(2, 1))
---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('MovingObjectDetector')
+sys.path.append('TrainNetwork')
+sys.path.append('SimpleTracker')
+
+import os
+from BaseFunctions2 import createImageDirectory
+from run_detection_main import run_detection_main
+
+def main():
+
+    attack = False 
+
+    input_image_idx = 10
+    ROI_centre = [4500, 5000]
+
+    model_folder = "Models/"
+    imagefolder = "/Users/xiaowei/Dropbox/wasabi-detection-python-new/WAPAFB_images_train/training/"
+    writeimagefolder0 = "/Users/xiaowei/Dropbox/temporary_buffer/code/savefig/"
+
+    exampleString = "%s_%s_%s/"%(input_image_idx,ROI_centre[0],ROI_centre[1])
+    createImageDirectory(writeimagefolder0+exampleString)
+
+    if attack: 
+        writeimagefolder = writeimagefolder0+exampleString+"attacked/"
+    else: 
+        writeimagefolder = writeimagefolder0+exampleString+"original/"
+    createImageDirectory(writeimagefolder)
+    
+    run_detection_main(attack,model_folder,imagefolder,input_image_idx,ROI_centre,writeimagefolder) 
+
+
+if __name__=="__main__":
+  main()
+
+
