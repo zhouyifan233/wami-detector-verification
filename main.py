1043e0a5625114d9ccb0ea069f54a26393bbccb1
Xiaowei Huang <xiaowei@xiaoweis-mbp.home>
2019-06-14 20:48:33 +0100

    update

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import sys
 import random
 import argparse
-# sys.path.append('MovingObjectDetector')
-# sys.path.append('TrainNetwork')
-# sys.path.append('SimpleTracker')
-# Specify DeepConcolic src file here:
-# sys.path.append('../DeepConcolic/src')
-from MovingObjectDetector.MOD_BaseFunctions import createImageDirectory
-from MovingObjectDetector.run_detection_main import run_detection_main
+
+if sys.platform == 'darwin':
+    sys.path.append('MovingObjectDetector')
+    sys.path.append('TrainNetwork')
+    sys.path.append('SimpleTracker')
+    #Specify DeepConcolic src file here:
+    sys.path.append('../DeepConcolic/src')
+    from MOD_BaseFunctions import createImageDirectory
+    from run_detection_main import run_detection_main
+else:
+    from MovingObjectDetector.MOD_BaseFunctions import createImageDirectory
+    from MovingObjectDetector.run_detection_main import run_detection_main
 
 
 def running(attack, model_folder, imagefolder, input_image_idx, ROI_centre, writeimagefolder0, ROI_window, num_of_template):
@@ -31,13 +36,15 @@ def main():
                       help="attack or not")
     parser.add_argument("--ROI_centre", dest="ROI_centre", default="4500, 5000",
                       help="ROI_centre")
-    #parser.add_argument("--output-image-folder", dest="writeimagefolder0", default="../savefig/",
-    #                  help="ROI_centre")
-    parser.add_argument("--output-image-folder", dest="writeimagefolder0", default="C:/Workspace-python/savefig/",
+    parser.add_argument("--output-image-folder", dest="writeimagefolder0", default="../savefig/",
                       help="ROI_centre")
-    #parser.add_argument("--wasabi-image-folder", dest="imagefolder", default="/Users/xiaowei/Dropbox/wasabi-detection-python-new/WAPAFB_images_train/training/",
+    #parser.add_argument("--output-image-folder", dest="writeimagefolder0", default="C:/Workspace-python/savefig/",
     #                  help="ROI_centre")
-    parser.add_argument("--wasabi-image-folder", dest="imagefolder", default="C:/WPAFB-images/training/",
+    parser.add_argument("--wasabi-image-folder", dest="imagefolder", default="/Users/xiaowei/Dropbox/wasabi-detection-python-new/WAPAFB_images_train/training/",
+                      help="ROI_centre")
+    #parser.add_argument("--wasabi-image-folder", dest="imagefolder", default="C:/WPAFB-images/training/",
+    #                  help="ROI_centre")
+    parser.add_argument("--deepconcolic-folder", dest="deepconcolic", default="../DeepConcolic/src/",
                       help="ROI_centre")
     parser.add_argument(
       '--ROI_window', dest='ROI_window', default='1000', help='the windows size of ROI')
@@ -46,7 +53,7 @@ def main():
 
     args = parser.parse_args()
 
-    attack = args.attack  
+    attack = args.attack
     input_image_idx = int(''.join(x for x in args.input_image_idx if x.isdigit()))
     ROI_window = int(''.join(x for x in args.ROI_window if x.isdigit()))
     num_of_template = int(''.join(x for x in args.num_of_template if x.isdigit()))
